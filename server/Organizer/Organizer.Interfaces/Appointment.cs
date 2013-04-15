281b7b695b0ebc4a139d3cdf5d05bc66c87feb72
tobias.lindener@gmail.com <tobias.lindener@gmail.com@e9c0e2af-fa3d-8481-e6ee-7703e61f93dc>
2013-04-15 15:45:05 +0000

    Added Appointment for multiuser calendar entries Added multicalendar feature to user

---
+++ server/Organizer/Organizer.Interfaces/Appointment.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Organizer.Interfaces
+{
+    public class Appointment : CalendarEntry
+    {
+
+        public Appointment()
+        {
+            Invitees = new List<User>();
+        }
+        public virtual List<User> Invitees { get; set; }
+
+
+        public bool IsEmpty
+        {
+            get
+            {
+                if (Invitees.Count == 0)
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+
+    }
+}
