09c687ac9bf39aab2dba86d6f4695cd511d138f7
blumenthal.jenny@googlemail.com <blumenthal.jenny@googlemail.com@e9c0e2af-fa3d-8481-e6ee-7703e61f93dc>
2013-04-24 13:32:14 +0000

    Version 1.0

---
+++ client/Organizer/src/DataPusher.java
@@ -0,0 +1,12 @@
+import java.util.Calendar;
+import java.util.Date;
+
+
+public interface DataPusher {
+//	gibt ein Objekt zurück in dem jeder Uhrzeit (0-23.30, halbstündliche Einteilung) eine Beschreibung zugeordnet ist
+	public Object[][] getBeschreibungen();
+	public String getDetails(String aktZeit);
+//	gibt die Details zur jeweiligen Beschreibung zurück
+	
+
+}
