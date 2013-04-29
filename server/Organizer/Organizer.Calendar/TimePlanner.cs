c7d84f1fac2c7889aba69b3a0d12174202d00f83
tobias.lindener@gmail.com <tobias.lindener@gmail.com@e9c0e2af-fa3d-8481-e6ee-7703e61f93dc>
2013-04-29 10:23:01 +0000

    Added a bunch of new methods

--- server/Organizer/Organizer.Calendar/TimePlanner.cs
+++ server/Organizer/Organizer.Calendar/TimePlanner.cs
@@ -8,19 +8,19 @@ using System.Threading.Tasks;
 
 namespace Organizer
 {
-    public class TimePlanner
+    public class TimePlanner 
     {
         CalendarContext calendarDatabase;
 
         public TimePlanner()
         {
             calendarDatabase = new CalendarContext();
-                 }
+        }
 
         #region Calendar
         public bool AddNewCalendar(Calendar calendar)
         {
-            
+
             if (ValidationMethods.isCalendarValid(calendar))
             {
                 calendarDatabase.Calendar.Add(calendar);
@@ -52,11 +52,26 @@ namespace Organizer
             calendar.CalendarEntries.Add(entry);
             calendarDatabase.SaveChanges();
             return true;
-        
+
         }
         public ICollection<CalendarEntry> GetAllEntriesByOwner(int ownerId)
         {
             return calendarDatabase.CalendarEntries.Where(p => p.Owner.UserId == ownerId).ToList();
+        }
+        public ICollection<CalendarEntry> GetEntriesByRoom(int roomId)
+        { 
+            var room = calendarDatabase.Rooms.Find(roomId);
+    
+            if (room == null)
+            {
+                return null;
+            }
+            return calendarDatabase.CalendarEntries.Where(p => p.Room == room).ToList();
+        }
+        public bool RemoveEntryFromCalendar(int calendarId, int entryId)
+        {
+            var entry = calendarDatabase.CalendarEntries.Find(entryId);
+            return calendarDatabase.Calendar.Find(calendarId).CalendarEntries.Remove(entry);
         }
         #endregion
 
@@ -76,17 +91,36 @@ namespace Organizer
         }
         #endregion
 
-
+        #region Rooms
+        public ICollection<Room> GetAllRooms()
+        {
+            return calendarDatabase.Rooms.ToList();
+        }
         public Room GetRoomById(int roomId)
         {
             return calendarDatabase.Rooms.Find(roomId);
         }
 
-        public bool RemoveEntryFromCalendar(int calendarId, int entryId)
+        #endregion
+        #region Groups
+        public ICollection<Group> GetAllGroups()
         {
-            var entry =  calendarDatabase.CalendarEntries.Find(entryId);
-            return calendarDatabase.Calendar.Find(calendarId).CalendarEntries.Remove(entry);
+            return calendarDatabase.Groups.ToList();
         }
+        public Group GetGroupById(int groupId)
+        {
+            return calendarDatabase.Groups.Find(groupId);
+        }
+        public ICollection<Group> GetGroupsByUserId(int userId)
+        {
+            var member = calendarDatabase.User.Find(userId);
+            if (member == null)
+            {
+                return null;
+            }
+            return calendarDatabase.Groups.Where(p => p.Members == member).ToList();
+        }
+        #endregion
     }
 
     public class CalendarContext : DbContext
