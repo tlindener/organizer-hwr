b73451d4771a33d62bd9c16b0af81abe61238408
blumenthal.jenny@googlemail.com <blumenthal.jenny@googlemail.com@e9c0e2af-fa3d-8481-e6ee-7703e61f93dc>
2013-05-02 08:44:13 +0000

    Version 1.3 Befüllung mit eigenen Daten problemlos möglich und getestet

--- trunk/client/Organizer/src/DataPusher.java
+++ trunk/client/Organizer/src/DataPusher.java
@@ -1,11 +1,13 @@
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 
 public interface DataPusher {
 //	gibt ein Objekt zurück in dem jeder Uhrzeit (0-23.30, halbstündliche Einteilung) eine Beschreibung zugeordnet ist
 	public Object[][] getBeschreibungen();
 	public String getDetails(String aktZeit);
+	public List getPersonen(String zeit);
 	public Object getDauer(String zeit);
 	public void setDauer();
 //	gibt die Details zur jeweiligen Beschreibung zurück
