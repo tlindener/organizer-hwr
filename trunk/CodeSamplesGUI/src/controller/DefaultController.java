6738427f81c67298268d53987942b875be0d5774
steffen.baumann91@gmail.com <steffen.baumann91@gmail.com@e9c0e2af-fa3d-8481-e6ee-7703e61f93dc>
2013-06-05 12:19:16 +0000

    CodeSamples für die Gui für Jenny

---
+++ trunk/CodeSamplesGUI/src/controller/DefaultController.java
@@ -0,0 +1,76 @@
+/**
+ * 
+ */
+package controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import view.window_TerminBearbeiten;
+import data.DataPusher;
+import data.model.MyModel;
+import data.objects.types.Room;
+import data.objects.types.User;
+
+/**
+ * @author Steffen Baumann
+ * @version 1.0 
+ *
+ */
+public class DefaultController implements DataPusher, ActionListener{
+
+	MyModel model = new MyModel();
+	window_TerminBearbeiten editEntry = null;
+	/**
+	 * 
+	 */
+	public DefaultController() {
+		//TODO some Stuff to build up a connection
+		model.refreshData();
+		startEditEntryWindow();
+		
+		
+	}
+
+	private void startEditEntryWindow() {
+		editEntry = new window_TerminBearbeiten(this, this);
+		editEntry.openEmptyFrame();
+		
+//		Willst du Daten schon vorher mitgeben, nutze einfach die Methode
+//		editEntry.openFrameWithValues(startTime, endTime, beschreibung, details);
+//		Vorselektionen habe ich jetzt noch nicht drinne, kann ich dir dann aber bei helfen ;-)
+		
+//		Das war der alte Anfang deines Codes im Controller --> kann alles weg soweit
+//		Vector<User> listUser = new Vector<User>();
+//		for (Object refObj : myModel.createAllePersonen()) {
+//			User u = (User) refObj;
+//			listUser.add(u);
+//		}
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new DefaultController();
+	}
+
+	@Override
+	public User[] pushUserList() {
+		return model.getListUser();
+	}
+
+	@Override
+	public Room[] pushRoomList() {
+		return model.getListRoom();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource().equals(editEntry.getBtnTerminEintragen())){
+			System.out.println("JA - richtiger BUTTON");
+//			TODO: Daten aus dem Fenster abfragen und auf setVisible(false) setzen.
+//			editEntry.getTxtBeschreibung()
+		}
+	}
+}
