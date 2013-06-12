c09acb5b6d13c7ef1cfb78f2fbc849ca6c3e3af6
tobias.lindener@gmail.com <tobias.lindener@gmail.com@e9c0e2af-fa3d-8481-e6ee-7703e61f93dc>
2013-06-12 22:04:06 +0000

    fixed parsing from json string

--- trunk/client/NetworkLayer/src/network/objects/NetDateTimeAdapter.java
+++ trunk/client/NetworkLayer/src/network/objects/NetDateTimeAdapter.java
@@ -1,8 +1,6 @@
 package network.objects;
 
 import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import com.google.gson.TypeAdapter;
@@ -26,43 +24,15 @@ public class NetDateTimeAdapter extends TypeAdapter<Date> {
             reader.nextNull();
             return null;
         }
-  
-        Date result = null;
-        String str = reader.nextString();
-        int start = str.indexOf("(") + 1;
-        int plus = str.indexOf("+");
-        int end = str.indexOf(")");
+        String time = reader.nextString();
+        Date date = Utils.parseStringToDate(time);
         
-        String millis = str.substring(start, plus);
-        String offset = str.substring(plus, end);
-        
-        result = new Date(Long.parseLong(millis));
-		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS"+offset);
-		String tmp = format.format(result);
-		try {
-			format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
-			result = format.parse(tmp);
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
-		
-		System.err.println("Das geparste Datum ist: " + result);
-		
-//        str = str.replaceAll("([+][0-9]{4})|([^0-9])", "");
-//        if (!str.equals("")) {
-//            try {
-//                result = new Date(Long.parseLong(str));
-//            } catch (NumberFormatException e) {
-//            }
-//        }
-        
-        
-        return result;
+        return date;
     }
 	@Override
 	public void write(JsonWriter out, Date value) throws IOException, UnsupportedOperationException {
 		String date = Utils.parseDateToNetDateTime(value);
-		System.out.println(date);
+		date = Utils.parseStringToHTTP(date);
 		out.value(date);
 	}
 
