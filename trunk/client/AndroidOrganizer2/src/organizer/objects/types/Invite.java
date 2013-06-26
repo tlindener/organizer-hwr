ffa6782947f452328e3e7db2ab76c88261ebdacc
tobias.lindener@gmail.com <tobias.lindener@gmail.com@e9c0e2af-fa3d-8481-e6ee-7703e61f93dc>
2013-06-26 21:36:27 +0000

    Changed to latest NetworkLayer Fixed bug with empty room

---
+++ trunk/client/AndroidOrganizer2/src/organizer/objects/types/Invite.java
@@ -0,0 +1,76 @@
+/**
+ * 
+ */
+package organizer.objects.types;
+
+import java.util.List;
+
+import organizer.objects.AbstractOrganizerObject;
+
+/**
+ * @author Steffen Baumann
+ * @version 1.0
+ * 
+ */
+public class Invite extends AbstractOrganizerObject {
+
+//	public static final String OWNER_ID = "OwnerId";
+//	public static final String CALENDAR_ENTRY_ID = "CalendarEntryId";
+	
+	private int userId = 0;
+	private int accepted = 0;
+	private int calendarEntryId = 0;
+
+	/**
+	 * @return the ownerId
+	 */
+	public int getOwnerId() {
+		return userId;
+	}
+
+	/**
+	 * @param ownerId the ownerId to set
+	 */
+	public void setOwnerId(int ownerId) {
+		this.userId = ownerId;
+	}
+
+	/**
+	 * @return the accepted
+	 */
+	public int isAccepted() {
+		return accepted;
+	}
+
+	/**
+	 * @param accepted the accepted to set
+	 */
+	public void setAccepted(int accepted) {
+		this.accepted = accepted;
+	}
+
+	/**
+	 * @return the calendarEntryId
+	 */
+	public int getCalendarEntryId() {
+		return calendarEntryId;
+	}
+
+	/**
+	 * @param calendarEntryId the calendarEntryId to set
+	 */
+	public void setCalendarEntryId(int calendarEntryId) {
+		this.calendarEntryId = calendarEntryId;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see organizer.objects.AbstractOrganizerObject#getProperty()
+	 */
+	@Override
+	public String[] getProperty() throws IllegalArgumentException {
+			throw new IllegalArgumentException(byProperty + " is an unkown Property for " + this.getClass().getSimpleName());
+	}
+
+}
---
+++ trunk/client/AndroidOrganizer2/src/organizer/objects/types/User.java
@@ -0,0 +1,124 @@
+/**
+ * 
+ */
+package organizer.objects.types;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import organizer.objects.AbstractOrganizerObject;
+
+/**
+ * This class represents an user from the database with all attributes except the password
+ * @author Steffen Baumann
+ */
+public class User extends AbstractOrganizerObject implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5502563591811214389L;
+	/** the surname of the user */
+	private String surname = "";
+	/** the given name of the user */
+	private String givenName = "";
+	/** the mail address of the user */
+	private String mailAddress = "";
+	/** the phone number of the user */
+	private String phoneNumber = "";
+	/** a list of all group IDs the user participates */
+	private List<Integer> groupIds = new ArrayList<Integer>();
+	/** a list of all calendar IDs the user has */
+	private List<Integer> calendarIds = new ArrayList<Integer>();
+	/** a list of all invite IDs the user has */
+	private List<Integer> inviteIds = new ArrayList<Integer>();
+	
+	/**
+	 * @return the surname
+	 */
+	public String getSurname() {
+		return surname;
+	}
+	/**
+	 * @param surname the surname to set
+	 */
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	/**
+	 * @return the givenName
+	 */
+	public String getGivenName() {
+		return givenName;
+	}
+	/**
+	 * @param givenName the givenName to set
+	 */
+	public void setGivenname(String givenName) {
+		this.givenName = givenName;
+	}
+	/**
+	 * @return the mailAddress
+	 */
+	public String getMailAddress() {
+		return mailAddress;
+	}
+	/**
+	 * @param mailAddress the mailAddress to set
+	 */
+	public void setMailAddress(String mailAddress) {
+		this.mailAddress = mailAddress;
+	}
+	/**
+	 * @return the phoneNumber
+	 */
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	/**
+	 * @param phoneNumber the phoneNumber to set
+	 */
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	/**
+	 * @return the groupIds
+	 */
+	public List<Integer> getGroupIds() {
+		return groupIds;
+	}
+	/**
+	 * @param groupIds the groupIds to set
+	 */
+	public void setGroupIds(List<Integer> groupIds) {
+		this.groupIds = groupIds;
+	}
+	/**
+	 * @return the calendarIds
+	 */
+	public List<Integer> getCalendarIds() {
+		return calendarIds;
+	}
+	/**
+	 * @param calendarIds the calendarIds to set
+	 */
+	public void setCalendarIds(List<Integer> calendarIds) {
+		this.calendarIds = calendarIds;
+	}
+	/**
+	 * @return the inviteIds
+	 */
+	public List<Integer> getInviteIds() {
+		return inviteIds;
+	}
+	/**
+	 * @param inviteIds the inviteIds to set
+	 */
+	public void setInviteIds(List<Integer> inviteIds) {
+		this.inviteIds = inviteIds;
+	}
+	@Override
+	public String[] getProperty() throws IllegalArgumentException {
+		throw new IllegalArgumentException("No available Property for " + getClass().getSimpleName());
+	}
+}
