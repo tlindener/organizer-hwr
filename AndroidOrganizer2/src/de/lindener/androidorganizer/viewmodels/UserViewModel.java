518a817286f2d784d6149c19b4ed6eaee2da5c12
tobias.lindener@gmail.com <tobias.lindener@gmail.com@e9c0e2af-fa3d-8481-e6ee-7703e61f93dc>
2013-07-02 13:15:11 +0000

    Create multithreading branch

---
+++ AndroidOrganizer2/src/de/lindener/androidorganizer/viewmodels/UserViewModel.java
@@ -0,0 +1,53 @@
+package de.lindener.androidorganizer.viewmodels;
+
+public class UserViewModel {
+
+        String givenName = null;        
+     String surname =null;         
+     String mailAddress = null;
+     String phoneNumber = null;
+     
+     public UserViewModel()
+     {
+    	 
+     }
+     
+     public UserViewModel(String givenName,String surname,String mailAddress,String phoneNumber)
+     {
+    	this.givenName = givenName;
+    	this.surname=surname;
+    	this.mailAddress = mailAddress;
+     }
+
+	public String getGivenName() {
+		return givenName;
+	}
+
+	public void setGivenName(String givenName) {
+		this.givenName = givenName;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getMailAddress() {
+		return mailAddress;
+	}
+
+	public void setMailAddress(String mailAddress) {
+		this.mailAddress = mailAddress;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+}
