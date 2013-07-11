02222411c7db521a61f17893ed5fdc1fe7afca97
steffen.baumann91@gmail.com <steffen.baumann91@gmail.com@e9c0e2af-fa3d-8481-e6ee-7703e61f93dc>
2013-07-11 08:28:17 +0000

    Last comments

--- trunk/client/NetworkLayer/src/organizer/objects/AbstractOrganizerObject.java
+++ trunk/client/NetworkLayer/src/organizer/objects/AbstractOrganizerObject.java
@@ -2,7 +2,6 @@ package organizer.objects;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
